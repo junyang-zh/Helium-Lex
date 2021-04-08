b833f1fe7e09d6c115e8e96a7fc628973e3be153
James-Hen <james-hen@outlook.com>
2021-04-08 16:47:30 +0800

    Almost finished developing, debug needed

--- lib/DFA.h
+++ lib/DFA.h
@@ -7,6 +7,7 @@
 #define DFAH
 
 #include <string>
+#include <cstring>
 #include <map>
 #include <vector>
 
@@ -18,6 +19,7 @@ class DFA {
 private:
     struct node {
         std::map<char, node*> edge;
+        node * och_default;
         std::string TokenType;
         int nID, TokenID; // -1 indicate inacceptable states
         node(int n) : nID(n), edge(), TokenType(), TokenID(-1) {}
@@ -25,6 +27,8 @@ private:
     } *S;
     node *now;
     int N, TKN; // node count, token count
+    node * tknn, // identifier
+        * rstn;
     void dfs_destruct(bool vis[], node *n) {
         vis[n->nID] = true;
         for (auto ch : n->edge) {
@@ -33,42 +37,142 @@ private:
         delete n;
         return;
     }
-    void trie_build(std::string str, std::string tnm) {
-        node *work(S);
-        for (auto ch : str) {
-            if (!(work->edge.count(ch))) {
-                work->edge[ch] = new node(N++);
-            }
-            work = work->edge[ch];
-        }
-        work->TokenID = TKN++;
-        work->TokenType = tnm;
-    }
 public:
     DFA() {
         N = 0;
         TKN = 0;
         now = S = new node(N++);
+        S->och_default = S;
+        tknn = new node(N++, "Identifier", TKN++);
+        rstn = new node(N++, "Identifier", TKN++);
+        S->edge['_'] = tknn;
+        tknn->edge['_'] = rstn;
+        tknn->och_default = S;
+        rstn->edge['_'] = rstn;
+        rstn->och_default = S;
     }
     ~DFA() {
         bool *vis = new bool [N];
+        memset(vis, 0, sizeof(vis));
         dfs_destruct(vis, S);
         delete [] vis;
     }
-    void add_LEX_rule() {
-        
-    }
-    void add_new_Words_Trie(WordsVec src, std::string name) {
-        for (auto str : src) {
-            trie_build(str, name);
+    void add_number_rule() { // [0-9]*(.[0-9]*)+
+        node * stnn = new node(N++, "Integer", TKN++),
+            * flt = stnn->edge['.'] = new node(N++, "Float   ", TKN++);
+        stnn->och_default = S;
+        flt->och_default = S;
+        for (char i('0'); i <= '9'; ++i) {
+            S->edge[i] = stnn;
+            stnn->edge[i] = stnn;
+            flt->edge[i] = flt;
         }
     }
-    void next(char a) {
-        if (!(now->edge.count(a))) {
+    void add_token_rule() { // [a-zA-Z][a-zA-z0-9_]*
+        for (char i('a'); i <= 'z'; ++i) {
+            if (S->edge.count(i)) continue;
+            S->edge[i] = tknn;
+        }
+        for (char i('a'); i <= 'z'; ++i) {
+            tknn->edge[i] = rstn;
+            rstn->edge[i] = rstn;
+        }
+        for (char i('A'); i <= 'Z'; ++i) {
+            if (S->edge.count(i)) continue;
+            S->edge[i] = tknn;
+        }
+        for (char i('A'); i <= 'Z'; ++i) {
+            tknn->edge[i] = rstn;
+            rstn->edge[i] = rstn;
+        }
+        for (char i('0'); i <= '9'; ++i) {
+            tknn->edge[i] = rstn;
+            rstn->edge[i] = rstn;
+        }
+    }
+    void add_str_rule() { // ['"][]*['"]
+        node * stn = new node(N++), *fina = new node(N++, "String  ", TKN++);
+        S->edge['\"'] = stn;
+        stn->och_default = stn;
+        stn->edge['\"'] = fina;
+        // now for ' do again
+        stn = new node(N++);
+        S->edge['\''] = stn;
+        stn->och_default = stn;
+        stn->edge['\''] = fina;
+
+        fina->och_default = S;
+    }
+    void add_new_OP_Trie(WordsVec src, std::string tnm) {
+        for (auto str : src) {
+            node *work(S);
+            for (auto ch : str) {
+                if (!(work->edge.count(ch))) {
+                    work->edge[ch] = new node(N++);
+                    work->edge[ch]->och_default = S;
+                }
+                work = work->edge[ch];
+            }
+            work->TokenID = TKN++;
+            work->TokenType = tnm;
+        }
+    }
+    void add_new_KW_Trie(WordsVec src, std::string tnm) {
+        for (auto str : src) {
+            node *work(S);
+            for (auto ch : str) {
+                if (!(work->edge.count(ch)) || work->edge[ch] == rstn) {
+                    work->edge[ch] = new node(N++);
+                    work = work->edge[ch];
+
+                    work->och_default = S;
+                    work->TokenID = TKN++;
+                    work->TokenType = "Identifier";
+                    work->edge['_'] = rstn;
+                    for (char i('a'); i <= 'z'; ++i) {
+                        work->edge[i] = rstn;
+                    }
+                    for (char i('A'); i <= 'Z'; ++i) {
+                        work->edge[i] = rstn;
+                    }
+                    for (char i('0'); i <= '9'; ++i) {
+                        work->edge[i] = rstn;
+                    }
+                }
+                else {
+                    work = work->edge[ch];
+                }
+            }
+            work->TokenType = tnm;
+        }
+    }
+    std::pair<int, std::string> next(char a) { // -1 continue, -2 is tab, -3 ignore
+        printf("%c %d %d %d \"%s\"\n", a, (void*)S, (void*)(now->och_default), now->TokenID, now->TokenType.c_str());
+        if (now == S && a == '\t') {
+            return std::make_pair(-2, "Indent");
+        }
+        else if (now == S && (a == ' ' || a == '\n' || a == '\r')) {
+            return std::make_pair(-3, "");
+        }
+        else if (now->edge.count(a) && now->edge[a] == S || !(now->edge.count(a)) && now->och_default == S) {
+            auto res = std::make_pair(now->TokenID, now->TokenType);
+            now = S;
+            if (now->edge.count(a)) {
+                now = now->edge[a];
+            }
+            return res;
+        }
+        else if (now->edge.count(a)) {
             now = now->edge[a];
+            return std::make_pair(-1, "");
+        }
+        else if (now->och_default != S) {
+            now = now->och_default;
+            return std::make_pair(-1, "");
         }
         else {
-            now = S;
+            printf("FATAL! next fail!\n");
+            exit(0);
         }
     }
 };
--- src/Main.cpp
+++ src/Main.cpp
@@ -7,6 +7,8 @@
 #include <string>
 #include <cstring>
 
+#define DEBUG(x) (printf("DEBUG:%d\n", x))
+
 #include "LexPy.h"
 #include "DFA.h"
 
@@ -43,7 +45,13 @@ int main(int argc, char *argv[], char *envp[]) {
     // preprocessing
     char *current_line = new char[Max_Line_Length];
     while (fgets(current_line, Max_Line_Length, InFile) != NULL) {
-        fputs(current_line, OutPreFile);
+        for (int i(0); current_line[i]; ++i) {
+            if (current_line[i] == '#') {
+                fputc('\n', OutPreFile);
+                break;
+            }
+            fputc(current_line[i], OutPreFile);
+        }
     }
     delete[] current_line;
 
@@ -54,14 +62,48 @@ int main(int argc, char *argv[], char *envp[]) {
         * OutLexFile = fopen(f_out[1], "w+");
 
     Helium::DFA LEXAUTO;
-    LEXAUTO.add_new_Words_Trie(Helium::PyKWList, "Keyword");
-    LEXAUTO.add_new_Words_Trie(Helium::PySingOpList, "OP");
-    LEXAUTO.add_new_Words_Trie(Helium::PyCompOpList, "OP");
-    int curr; // now char
+    LEXAUTO.add_new_KW_Trie(Helium::PyKWList, "Keyword");
+    LEXAUTO.add_new_OP_Trie(Helium::PySingOpList, "Operator");
+    LEXAUTO.add_new_OP_Trie(Helium::PyCompOpList, "Operator");
+    LEXAUTO.add_number_rule();
+    LEXAUTO.add_str_rule();
+    LEXAUTO.add_token_rule();
+    int WordCnt(0), nl(0), curr; // now char
+    char * currtoken = new char[Max_Line_Length];
     while ((curr = fgetc(InPreFile)) != EOF) {
+        auto res = LEXAUTO.next(curr);
+        printf("chk %d\n", res.first);
+        if (res.first == -1) { // moving
+            currtoken[nl++] = curr;
+        }
+        else if (res.first == -2) { // \t
+            currtoken[nl] = '\0';
+            fputs("<\'\\t\',Indent>\n", OutLexFile);
+            currtoken[0] = curr;
+            nl = 1;
+        }
+        else if (res.first == -3) { // \n\r
+            continue;
+        }
+        else { // if >= 0 // TODO solve finished asssss
+            currtoken[nl] = '\0';
+            fprintf(OutLexFile, "%04d\t", WordCnt++);
+            fputs(res.second.c_str(), OutLexFile);
+            fputs(",\t\t\t", OutLexFile);
+            fputs(currtoken, OutLexFile);
+            fputs("\n", OutLexFile);
+            if (curr == ' ' || curr == '\n' || curr == '\r') {
+                nl = 0;
+            }
+            else {
+                currtoken[0] = curr;
+                nl = 1;
+            }
+        }
+    };
+    delete[] currtoken;
 
-    }
-
+    fclose(InPreFile);
     fclose(OutLexFile);
 
     return 0;
